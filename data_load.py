f982b5b5bb8461dda3eeacd06565ae27d32f89e7
Sungyoon Lee <30929338+sungyoon-lee@users.noreply.github.com>
2020-10-14 15:35:22 +0900

    Update data_load.py

--- data_load.py
+++ data_load.py
@@ -16,99 +16,34 @@ from torch import autograd
 
 from torch.utils.data import Dataset, DataLoader, TensorDataset
 from torch.optim.lr_scheduler import StepLR, MultiStepLR
+import utils, data_load, BCP
 
-
-def data_loaders(data, batch_size, augmentation=False, normalization=False, drop_last=False, shuffle=True): #############
-
-    if not os.path.exists('data/'):
-        os.makedirs('data/')
-
-    if data == 'cifar10': 
-        if augmentation and normalization:
-            trainset_transforms = transforms.Compose([
-                transforms.RandomCrop(32, padding=4),
-                transforms.RandomHorizontalFlip(),
-                transforms.ToTensor(),
-                transforms.Normalize((0.485, 0.456, 0.406), (0.225, 0.225, 0.225))])
-            trainset = datasets.CIFAR10(root='./data/', train=True, download=True, transform=trainset_transforms)
-            testset = datasets.CIFAR10(root='./data/', train=False, download=True, transform=transforms.Compose([
-                transforms.ToTensor(),
-                transforms.Normalize((0.485, 0.456, 0.406), (0.225, 0.225, 0.225))]))
-            
-        elif augmentation and not(normalization):
-            trainset_transforms = transforms.Compose([
-                transforms.RandomCrop(32, padding=4),
-                transforms.RandomHorizontalFlip(),
-                transforms.ToTensor()])
-            trainset = datasets.CIFAR10(root='./data/', train=True, download=True, transform=trainset_transforms)
-            testset = datasets.CIFAR10(root='./data/', train=False, download=True, transform=transforms.Compose([
-                transforms.ToTensor()]))
-            
-        else:
-            trainset = datasets.CIFAR10(root='./data/', train=True, download=True, transform=transforms.ToTensor())
-            testset = datasets.CIFAR10(root='./data/', train=False, download=True, transform=transforms.ToTensor())
-
-
-        
-    if data == 'mnist':
-        trainset = datasets.MNIST(root='./data/', train=True, download=True, transform=transforms.ToTensor())
-        testset = datasets.MNIST(root='./data/', train=False, download=True, transform=transforms.ToTensor())
+if __name__ == "__main__":
+    args = utils.argparser()
+    print(args)
+    setproctitle.setproctitle(args.prefix)
+    train_log = open(args.prefix + "_train.log", "w")
+    test_log = open(args.prefix + "_test.log", "w")
     
-    if data == 'tinyimagenet':
-        if augmentation and normalization:
-            trainset_transforms = transforms.Compose([
-                    transforms.RandomCrop(64, padding=4),
-                    transforms.RandomHorizontalFlip(),
-                    transforms.ToTensor(),
-                    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
-            trainset = datasets.ImageFolder(root='../../Imagenet/tiny-imagenet-200/train', transform=trainset_transforms)#, train=True, download=True
-            testset = datasets.ImageFolder(root='../../Imagenet/tiny-imagenet-200/val', transform=transforms.Compose([
-                    transforms.ToTensor(),
-                    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])]))
-            
-        elif not augmentation and normalization:
-            trainset_transforms = transforms.Compose([
-                    transforms.RandomHorizontalFlip(),
-                    transforms.ToTensor(),
-                    transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
-            testset_transforms = transforms.Compose([
-                    transforms.ToTensor()])
-            trainset = datasets.ImageFolder(root='../../Imagenet/tiny-imagenet-200/train', transform=trainset_transforms)#, train=True, download=True
-            testset = datasets.ImageFolder(root='../../Imagenet/tiny-imagenet-200/val', transform=testset_transforms)
-            
-        elif augmentation and not normalization: ##############################
-            trainset_transforms = transforms.Compose([
-                    transforms.RandomCrop(64, padding=4),
-                    transforms.RandomHorizontalFlip(),
-                    transforms.ToTensor()])
-            testset_transforms = transforms.Compose([
-                    transforms.ToTensor()])
-            trainset = datasets.ImageFolder(root='../../Imagenet/tiny-imagenet-200/train', transform=trainset_transforms)#, train=True, download=True
-            testset = datasets.ImageFolder(root='../../Imagenet/tiny-imagenet-200/val', transform=testset_transforms)
-            
-        elif not augmentation and not normalization:
-            trainset_transforms = transforms.Compose([
-                    transforms.RandomHorizontalFlip(),
-                    transforms.ToTensor()])
-            testset_transforms = transforms.Compose([
-                    transforms.ToTensor()])
-            trainset = datasets.ImageFolder(root='../../Imagenet/tiny-imagenet-200/train', transform=trainset_transforms)#, train=True, download=True
-            testset = datasets.ImageFolder(root='../../Imagenet/tiny-imagenet-200/val', transform=testset_transforms)
-        
-        
-#         testset.targets = list(np.array(testset.targets)-1) ########################
-        testset.target_transform = lambda id: id-1
+    _, test_loader = data_load.data_loaders(args.data, args.test_batch_size, args.normalization, args.augmentation, args.drop_last, args.shuffle)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
 
+    args.opt_max = 10000
+    args.print = True
+    t = 100
+    try:
+        aa = torch.load(args.test_pth)['state_dict'][0]
+    except:
+        aa = torch.load(args.test_pth)['state_dict']
 
-
-    data_loader  = torch.utils.data.DataLoader(dataset=trainset,
-                                              batch_size=batch_size,
-                                              shuffle=shuffle, 
-                                              drop_last = drop_last,
-                                              num_workers=1)
-    test_data_loader  = torch.utils.data.DataLoader(dataset=testset,
-                                              batch_size=batch_size,
-                                              shuffle=False,
-                                              num_workers=1)
-    
-    return data_loader, test_data_loader
+    model_eval = utils.select_model(args.data, args.model)
+    model_eval.load_state_dict(aa)
+    print('std testing ...')
+    std_err = utils.evaluate(test_loader, model_eval, t, test_log, args.verbose)
+    print('verification testing ...')
+    if args.method=='BCP':
+        last_err = BCP.evaluate_BCP(test_loader, model_eval, args.epsilon, t, test_log, args.verbose, args, None)
+    print('pgd testing ...')
+    pgd_err = utils.evaluate_pgd(test_loader, model_eval, args)
+    print('Best model evaluation:', std_err.item(), pgd_err.item(), last_err.item())
