49dd6682d98b2448005d2fad1e32ce2c6769055a
Sungyoon Lee <30929338+sungyoon-lee@users.noreply.github.com>
2020-10-16 17:30:36 +0900

    Update and rename main_mnist.py to train_mnist.py

--- train_mnist.py
+++ train_mnist.py
@@ -3,6 +3,7 @@
 ### basic modules
 import numpy as np
 import time, pickle, os, sys, json, PIL, tempfile, warnings, importlib, math, copy, shutil, setproctitle
+from datetime import datetime
 
 ### torch modules
 import torch
@@ -16,17 +17,18 @@ from torch import autograd
 
 from torch.utils.data import Dataset, DataLoader, TensorDataset
 from torch.optim.lr_scheduler import StepLR, MultiStepLR
-import utils, data_load, BCP
+import data_load, BCP, utils
 
 if __name__ == "__main__":
-    args = utils.argparser(data='mnist',epsilon=1.58,epsilon_infty=0.1,epochs=60,rampup=20,wd_list=[21,30,40],warmup=1,opt_iter=10,test_sniter=5000)
+    args = utils.argparser(data='mnist',epochs=60,warmup=1,rampup=20,batch_size=50,epsilon=1.58,epsilon_infty=0.1,epsilon_train=1.58,epsilon_train_infty=0.1,augmentation=False,lr=0.0003,lr_scheduler='multistep',wd_list=[21,30,40],gamma=0.1,opt_iter=10)
+    print(datetime.now())
     print(args)
     print('saving file to {}'.format(args.prefix))
     setproctitle.setproctitle(args.prefix)
     train_log = open(args.prefix + "_train.log", "w")
     test_log = open(args.prefix + "_test.log", "w")
-    train_loader, _ = data_load.data_loaders(args.data, args.batch_size, args.augmentation, args.normalization, args.drop_last, args.shuffle)
-    _, test_loader = data_load.data_loaders(args.data, args.test_batch_size, args.augmentation, args.normalization, args.drop_last, args.shuffle)
+    train_loader, _ = data_load.data_loaders(args.data, args.batch_size, augmentation=args.augmentation, normalization=args.normalization, drop_last=args.drop_last, shuffle=args.shuffle)
+    _, test_loader = data_load.data_loaders(args.data, args.test_batch_size, augmentation=args.augmentation, normalization=args.normalization, drop_last=args.drop_last, shuffle=args.shuffle)
     torch.manual_seed(args.seed)
     torch.cuda.manual_seed(args.seed)
 
@@ -37,19 +39,20 @@ if __name__ == "__main__":
     err = 1
     sampler_indices = []
     model = [utils.select_model(args.data, args.model)]
-    print(model[-1])
+    print(model[-1])    
     if args.opt == 'adam': 
         opt = optim.Adam(model[-1].parameters(), lr=args.lr)
     elif args.opt == 'sgd': 
         opt = optim.SGD(model[-1].parameters(), lr=args.lr, 
                         momentum=args.momentum,
                         weight_decay=args.weight_decay) 
+    print(opt)
     if args.lr_scheduler == 'step':
         lr_scheduler = optim.lr_scheduler.StepLR(opt, step_size=args.step_size, gamma=args.gamma)
     elif args.lr_scheduler =='multistep':
         lr_scheduler = MultiStepLR(opt, milestones=args.wd_list, gamma=args.gamma)
-    
-    eps_schedule = np.linspace(args.starting_epsilon, 
+    print(lr_scheduler)
+    eps_schedule = np.linspace(args.starting_epsilon,
                                args.epsilon_train,                                
                                args.schedule_length)
     
@@ -57,22 +60,27 @@ if __name__ == "__main__":
                                args.kappa,                                
                                args.kappa_schedule_length)
     u_list = None
-    for t in range(args.epochs):    
-            
+    for t in range(args.epochs): 
         if t < args.warmup:
             epsilon = 0
+            epsilon_next = 0
         elif args.warmup <= t < args.warmup+len(eps_schedule) and args.starting_epsilon is not None: 
             epsilon = float(eps_schedule[t-args.warmup])
+            epsilon_next = float(eps_schedule[np.min((t+1-args.warmup, len(eps_schedule)-1))])
         else:
-            epsilon = args.epsilon
+            epsilon = args.epsilon_train
+            epsilon_next = args.epsilon_train
             
         if t < args.warmup:
             kappa = 1
+            kappa_next = 1
         elif args.warmup <= t < args.warmup+len(kappa_schedule):
             kappa = float(kappa_schedule[t-args.warmup])
+            kappa_next = float(kappa_schedule[np.min((t+1-args.warmup, len(kappa_schedule)-1))])
         else:
             kappa = args.kappa
-        print('%.f th epoch: epsilon: %.3f, kappa: %.3f, lr: %.5f'%(t,epsilon,kappa,opt.state_dict()['param_groups'][0]['lr']))
+            kappa_next = args.kappa
+        print('%.f th epoch: epsilon: %.7f - %.7f, kappa: %.4f - %.4f, lr: %.7f'%(t,epsilon,epsilon_next,kappa,kappa_next,opt.state_dict()['param_groups'][0]['lr']))
             
         if t < args.warmup:
             utils.train(train_loader, model[-1], opt, t, train_log, args.verbose)
@@ -81,34 +89,38 @@ if __name__ == "__main__":
             st = time.time()
             u_list = BCP.train_BCP(train_loader, model[-1], opt, epsilon, kappa, t, train_log, args.verbose, args, u_list)
             print('Taken', time.time()-st, 's/epoch')
-                
-            err = BCP.evaluate_BCP(test_loader, model[-1], args.epsilon, t, test_log, args.verbose, args, u_list)
-            print("Robust error: %.4f, Best Robust error: %.4f"%(err.data.cpu().item(), best_err))
+            
+            err = BCP.evaluate_BCP(test_loader, model[-1], epsilon_next, t, test_log, args.verbose, args, u_list)
+            
             
         if args.lr_scheduler == 'step': 
-            lr_scheduler.step(epoch=max(t-len(eps_schedule)-args.warmup, 0))
+            if max(t - (args.rampup + args.warmup - 1) + 1, 0):
+                print("LR DECAY STEP")
+            lr_scheduler.step(epoch=max(t - (args.rampup + args.warmup - 1) + 1, 0))
         elif args.lr_scheduler =='multistep':
-            lr_scheduler.step(epoch=t)   
+            print("LR DECAY STEP")
+            lr_scheduler.step(epoch=t)      
+        else:
+            raise ValueError("Wrong LR scheduler")
             
-        if err < best_err and args.save: 
-            print('Best Error Found! %.3f'%err)
-            best_err = err
-            torch.save({
-                'state_dict' : [m.state_dict() for m in model], 
-                'err' : best_err,
+        if t>=args.warmup+len(eps_schedule):    
+            if err < best_err and args.save: 
+                print('Best Error Found! %.3f'%err)
+                best_err = err
+                torch.save({
+                    'state_dict' : [m.state_dict() for m in model], 
+                    'err' : best_err,
+                    'epoch' : t,
+                    'sampler_indices' : sampler_indices
+                    }, args.prefix + "_best.pth")
+
+            torch.save({ 
+                'state_dict': [m.state_dict() for m in model],
+                'err' : err,
                 'epoch' : t,
                 'sampler_indices' : sampler_indices
-                }, args.prefix + "_best.pth")
+                }, args.prefix + "_checkpoint.pth")  
 
-        torch.save({ 
-            'state_dict': [m.state_dict() for m in model],
-            'err' : err,
-            'epoch' : t,
-            'sampler_indices' : sampler_indices
-            }, args.prefix + "_checkpoint.pth")  
-        
-    args.sniter = 10000
-    args.opt_max = 20
     args.print = True
     
     aa = torch.load(args.prefix + "_best.pth")['state_dict'][0]
@@ -119,6 +131,6 @@ if __name__ == "__main__":
     print('pgd testing ...')
     pgd_err = utils.evaluate_pgd(test_loader, model_eval, args)
     print('verification testing ...')
-    last_err = BCP.evaluate_BCP(test_loader, model_eval, args.epsilon, t, test_log, args.verbose, args, u_list)
-    print('Best model evaluation:', last_err.item())
+    if args.method=='BCP':
+        last_err = BCP.evaluate_BCP(test_loader, model_eval, args.epsilon, t, test_log, args.verbose, args, u_list)  
     print('Best model evaluation:', std_err.item(), pgd_err.item(), last_err.item())
