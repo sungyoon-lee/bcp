2aabb0c386dca428bb3314cb205607253fab4dc2
Sungyoon Lee <30929338+sungyoon-lee@users.noreply.github.com>
2021-01-27 21:30:03 +0900

    Update evaluate_cifar10.py

--- evaluate_cifar10.py
+++ evaluate_cifar10.py
@@ -32,9 +32,9 @@ if __name__ == "__main__":
     t = 100
     
     try:
-        aa = torch.load(args.test_pth,map_location='cuda:0')['state_dict'][0]
+        aa = torch.load(args.test_pth,map_location='cuda:'+str(args.cuda_ids))['state_dict'][0]
     except:
-        aa = torch.load(args.test_pth,map_location='cuda:0')['state_dict']
+        aa = torch.load(args.test_pth,map_location='cuda:'+str(args.cuda_ids))['state_dict']
     model_eval = utils.select_model(args.data, args.model)
     model_eval.load_state_dict(aa)
     print('std testing ...')
